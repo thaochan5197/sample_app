73a8b196f0eea13c199f91a2bd12c2b19a7a728b
ThaoChan <thaochan5197@gmail.com>
2018-11-07 11:36:30 +0700

    chapter_12

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,11 @@
 class UserMailer < ApplicationMailer
   def account_activation user
     @user = user
-    mail to: user.email, subject: "Account activation"
+    mail to: user.email, subject: t("activation_account")
+  end
+
+  def password_reset user
+    @user = user
+    mail to: user.email, subject: t("password_reset")
   end
 end
